68cfc434740868e80943e2a87891c787ae98b4d2
ShintaKanayama <shinta327tuba@gmail.com>
2021-06-13 12:19:41 +0000

    File settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get '/home/about' => 'homes#about'
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show]
 end
