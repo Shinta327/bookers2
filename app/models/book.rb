86a15a92fbd4b6f56c278b8f0415b939b50f7392
ShintaKanayama <shinta327tuba@gmail.com>
2021-07-07 14:38:56 +0000

    [Finish]Ajax

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,7 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   def favorited_by?(user)
-    favorites.exists?(user_id: user.id)
+    favorites.where(user_id: user.id).exists?
   end
 
   validates :title, presence: true
